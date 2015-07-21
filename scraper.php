cf3f13c4121d5d378ee6b90d74d0db9d87461c60
Maxim <maxvanceffer@gmail.com>
2015-07-21 18:03:17 +0300

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -14,20 +14,31 @@ $dom->load($html);
 
 $items = $dom->find('.post-list-wrap .post-list-container-item');
 $posts = array();
+$unique_keys = array();
 
 foreach($items as $item) {
   $post = array();
   
+  $id = $item->getAttribute('data-id');
+  $result = scraperwiki::select("* from posts where 'id'='$id'");
+  print_r(json_encode($result));
+  
+  if(!is_null($result) && count($result) >= 1) continue;
+  
+  $post['id']    = $id
   $post['title'] = $item->find('a[itemprop="URL"]')[0]->text();
-  $post['id']    = $item->getAttribute('data-id');
   $post['image'] = $item->find('.post-list-container-item-img img')[0]->getAttribute('src');
   $post['description'] = $item->find('p[itemprop="description"]')[0]->text();
   $post['keywords'] = $item->find('div.post-list-container-item-text-info')[0]->find('span')[2]->text();
   
+  
   $posts[] = $post;
 }
 
-  print_r(json_encode($posts));
+$result = scraperwiki::save_sqlite($unique_keys, $posts, 'posts');
+
+print_r(json_encode($result));
+print_r(json_encode($posts));
 
 // print_r($dom->find("table.list"));
 //
